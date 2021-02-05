aa9bc7920781d2046c72afa032ef555411afff77
malek88888 <jmzomalek@gmail.com>
2021-02-05 09:17:53 +0200

    crud operation added to the home.componant

--- client-Angular/src/app/home/home.component.ts
+++ client-Angular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService} from '../http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+tasks:object={}
+allTasks:Array<Object>=[]
+body:Object={}
+  constructor(private http:HttpService) { }
 
-  constructor() { }
-  
   ngOnInit(): void {
+    this.http.getTasks().subscribe((data)=>{
+      this.tasks=data
+      console.log(data);
+  });
   }
-
+  addOneTask(){
+    this.http.addTasks(this.body).subscribe((data)=>console.log(data)
+    )
+  }
+  deleteOneTask(id:Number){
+    this.http.deleteTasks(id).subscribe((data)=>console.log(data)
+    )
+  }
+  updateOneTask(id:Number){
+    this.http.UpdateTasks(id).subscribe((data)=>console.log(data)
+    )
+  }
 }
--- client-Angular/src/app/http.service.ts
+++ client-Angular/src/app/http.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {}
+    getTasks(){
+      return this.http.get("http://localhost:4000/tasks")
+      }
+
+      addTasks(body:Object){
+        return this.http.post("http://localhost:4000/tasks",body)
+        }
+        deleteTasks(id:Number){
+          return this.http.delete(`http://localhost:4000/tasks/${id}`)
+          }
+          UpdateTasks(id:Number){
+            return this.http.put(`http://localhost:4000/tasks/${id}`,{})
+            }
 }
